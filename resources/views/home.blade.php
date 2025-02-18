287b24ae45d751de0fb4bdc8d4291d770af1e192
unknown <dan_bertrand@live.ca>
2025-02-18 17:09:18 -0500

    added pic to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,11 @@
 @section("content")
 
 <div id="content">
+
+
     <h2>Home</h2>
+    <img src="{{ asset('images/home_pic.png') }}" alt="Description">
+
     <p>This is the About page</p>
 </div>
 
